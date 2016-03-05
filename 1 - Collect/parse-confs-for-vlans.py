8882847882fa0528ed063054a7bf29044ceb9300
-_- <owl@rainbow-bat.local>
2016-03-05 20:26:56 +1100

    2

--- 1 - Collect/parse-confs-for-vlans.py
+++ 1 - Collect/parse-confs-for-vlans.py
@@ -108,7 +108,7 @@ while n<len(fg):
                     print "Can't parse",fg[n]
                     exit(1)
 
-outfileheadings= [ "sw_vlan_name", "fw_vlan_name", "vlan_id", "vlan_in_DC1", "vlan_in_DC2", "vlan_in_fw", "dc1-sw1_ip", "dc1-sw2_ip", "dc2-sw1_ip", "dc2-sw2_ip", "DC1_vrrp_ip", "DC2_vrrp_ip", "vrrp_ips_match", "fw_vdom", "fw_ip", "vlan_subnet" ]
+outfileheadings= [ "sw_vlan_name", "fw_vlan_name", "vlan_id", "vlan_in_DC1", "vlan_in_DC2", "vlan_in_fw", "dc1_sw1_ip", "dc1_sw2_ip", "dc2_sw1_ip", "dc2_sw2_ip", "DC1_vrrp_ip", "DC2_vrrp_ip", "vrrp_ips_match", "fw_vdom", "fw_ip", "vlan_subnet" ]
 outfilelist=[ ','.join(outfileheadings) ]
 
 all_vlan_ids=set()
@@ -132,17 +132,17 @@ for vlan_id in all_vlan_ids:
 
     vrrp_ips_set=set()
 
-    vrrp_ip_dc1-sw1=vlans["dc1-sw1"]["vlans"][vlan_id]["vlan_vrrp_ip"]
-    vrrp_ip_dc1-sw2=vlans["dc1-sw2"]["vlans"][vlan_id]["vlan_vrrp_ip"]
-    vrrp_ip_dc2-sw1=vlans["dc2-sw1"]["vlans"][vlan_id]["vlan_vrrp_ip"]
-    vrrp_ip_dc2-sw2=vlans["dc2-sw2"]["vlans"][vlan_id]["vlan_vrrp_ip"]
+    vrrp_ip_dc1_sw1=vlans["dc1-sw1"]["vlans"][vlan_id]["vlan_vrrp_ip"]
+    vrrp_ip_dc1_sw2=vlans["dc1-sw2"]["vlans"][vlan_id]["vlan_vrrp_ip"]
+    vrrp_ip_dc2_sw1=vlans["dc2-sw1"]["vlans"][vlan_id]["vlan_vrrp_ip"]
+    vrrp_ip_dc2_sw2=vlans["dc2-sw2"]["vlans"][vlan_id]["vlan_vrrp_ip"]
 
-    if vrrp_ip_dc1-sw1!=vrrp_ip_dc1-sw2 or vrrp_ip_dc2-sw1!=vrrp_ip_dc2-sw2:
+    if vrrp_ip_dc1_sw1!=vrrp_ip_dc1_sw2 or vrrp_ip_dc2_sw1!=vrrp_ip_dc2_sw2:
         print "VRRP IP mismatch!"
         for switch_name in switches:
             print "Switch",switch_name,"vlan",vlan_name,"vrrp_ip",vlans[switch_name]["vlans"][vlan_id]["vlan_vrrp_ip"]
-    vrrp_ip_DC1=vrrp_ip_dc1-sw1
-    vrrp_ip_DC2=vrrp_ip_dc2-sw1
+    vrrp_ip_DC1=vrrp_ip_dc1_sw1
+    vrrp_ip_DC2=vrrp_ip_dc2_sw1
 
     if vrrp_ip_DC1=="" or vrrp_ip_DC2=="":
         vrrp_ips_match=""
@@ -151,10 +151,10 @@ for vlan_id in all_vlan_ids:
     else:
         vrrp_ips_match="N"
 
-    vlan_name_dc1-sw1=vlans["dc1-sw1"]["vlans"][vlan_id]["vlan_name"]
-    vlan_name_dc1-sw2=vlans["dc1-sw2"]["vlans"][vlan_id]["vlan_name"]
-    vlan_name_dc2-sw1=vlans["dc2-sw1"]["vlans"][vlan_id]["vlan_name"]
-    vlan_name_dc2-sw2=vlans["dc2-sw2"]["vlans"][vlan_id]["vlan_name"]
+    vlan_name_dc1_sw1=vlans["dc1-sw1"]["vlans"][vlan_id]["vlan_name"]
+    vlan_name_dc1_sw2=vlans["dc1-sw2"]["vlans"][vlan_id]["vlan_name"]
+    vlan_name_dc2_sw1=vlans["dc2-sw1"]["vlans"][vlan_id]["vlan_name"]
+    vlan_name_dc2_sw2=vlans["dc2-sw2"]["vlans"][vlan_id]["vlan_name"]
     vlan_name_fw=vlans["fw"]["vlans"][vlan_id]["vlan_name"]
 
     vlan_in_DC1=""
@@ -166,19 +166,19 @@ for vlan_id in all_vlan_ids:
         print "\tSwitch",sw,"vlan name",vlans[sw]["vlans"][vlan_id]["vlan_name"]
         print "\t",vlans[sw]["vlans"][vlan_id]
 
-    if vlan_name_dc1-sw1!="" and vlan_name_dc1-sw1==vlan_name_dc1-sw2:
+    if vlan_name_dc1_sw1!="" and vlan_name_dc1_sw1==vlan_name_dc1_sw2:
         vlan_in_DC1="Y"
 
-    if vlan_name_dc2-sw1!="" and vlan_name_dc2-sw1==vlan_name_dc2-sw2:
+    if vlan_name_dc2_sw1!="" and vlan_name_dc2_sw1==vlan_name_dc2_sw2:
         vlan_in_DC2="Y"
 
     if vlan_name_fw!="":
         vlan_in_fw="Y"
 
-    if vlan_name_dc1-sw1!=vlan_name_dc1-sw2 and vlan_name_dc2-sw1==vlan_name_dc2-sw2=="":
-        if vlan_name_dc1-sw1=="":
+    if vlan_name_dc1_sw1!=vlan_name_dc1_sw2 and vlan_name_dc2_sw1==vlan_name_dc2_sw2=="":
+        if vlan_name_dc1_sw1=="":
             vlan_in_DC1="dc1-sw2 only"
-        elif vlan_name_dc1-sw2=="":
+        elif vlan_name_dc1_sw2=="":
             vlan_in_DC1="dc1-sw1 only"
         else:
             print"VLAN mismatch within DC1 site - ID",vlan_id
@@ -186,10 +186,10 @@ for vlan_id in all_vlan_ids:
                 print "\tSwitch",sw,"vlan name",vlans[sw]["vlans"][vlan_id]["vlan_name"]
             exit(1)
 
-    if vlan_name_dc2-sw1!=vlan_name_dc2-sw2 and vlan_name_dc1-sw1==vlan_name_dc1-sw2=="":
-        if vlan_name_dc2-sw1=="":
+    if vlan_name_dc2_sw1!=vlan_name_dc2_sw2 and vlan_name_dc1_sw1==vlan_name_dc1_sw2=="":
+        if vlan_name_dc2_sw1=="":
             vlan_in_DC2="dc2-sw2 only"
-        elif vlan_name_dc2-sw2=="":
+        elif vlan_name_dc2_sw2=="":
             vlan_in_DC2="dc2-sw1 only"
         else:
             print"VLAN mismatch within DC2 site - ID",vlan_id
@@ -211,23 +211,23 @@ for vlan_id in all_vlan_ids:
     if len(vlan_masks_set)==1:
         vlan_mask=vlan_masks_set.pop()
 
-    dc1-sw1_ip=vlans["dc1-sw1"]["vlans"][vlan_id]["vlan_ip"]
-    dc1-sw2_ip=vlans["dc1-sw2"]["vlans"][vlan_id]["vlan_ip"]
-    dc2-sw1_ip=vlans["dc2-sw1"]["vlans"][vlan_id]["vlan_ip"]
-    dc2-sw2_ip=vlans["dc2-sw2"]["vlans"][vlan_id]["vlan_ip"]
+    dc1_sw1_ip=vlans["dc1-sw1"]["vlans"][vlan_id]["vlan_ip"]
+    dc1_sw2_ip=vlans["dc1-sw2"]["vlans"][vlan_id]["vlan_ip"]
+    dc2_sw1_ip=vlans["dc2-sw1"]["vlans"][vlan_id]["vlan_ip"]
+    dc2_sw2_ip=vlans["dc2-sw2"]["vlans"][vlan_id]["vlan_ip"]
     fw_ip=vlans["fw"]["vlans"][vlan_id]["vlan_ip"]
     fw_vdom=vlans["fw"]["vlans"][vlan_id]["vlan_vdom"]
 
     vlan_ip=""
     vlan_subnet=""
-    for ip in [ dc1-sw1_ip, dc1-sw2_ip, dc2-sw1_ip, dc2-sw2_ip, fw_ip ]:
+    for ip in [ dc1_sw1_ip, dc1_sw2_ip, dc2_sw1_ip, dc2_sw2_ip, fw_ip ]:
         if ip!="" and ip:
             vlan_ip=ip
             break
     if vlan_ip!="":
         vlan_subnet=str(ip_network(unicode("%s/%s" % (vlan_ip, vlan_mask)),strict=False))
 
-    linelist=[ sw_vlan_name, fw_vlan_name, vlan_id, vlan_in_DC1, vlan_in_DC2, vlan_in_fw, dc1-sw1_ip, dc1-sw2_ip, dc2-sw1_ip, dc2-sw2_ip, vrrp_ip_DC1, vrrp_ip_DC2, vrrp_ips_match, fw_vdom, fw_ip, vlan_subnet ]
+    linelist=[ sw_vlan_name, fw_vlan_name, vlan_id, vlan_in_DC1, vlan_in_DC2, vlan_in_fw, dc1_sw1_ip, dc1_sw2_ip, dc2_sw1_ip, dc2_sw2_ip, vrrp_ip_DC1, vrrp_ip_DC2, vrrp_ips_match, fw_vdom, fw_ip, vlan_subnet ]
 
     outfilelist.append(','.join( linelist))
 
